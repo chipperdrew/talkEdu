1fca8b294872b6dafd9501f49dee38a342e40375
Andrew Cook <chipperdrew@gmail.com>
2013-06-09 12:24:52 -0600

    Posts app created, basic home page unit tests created

---
+++ posts/tests.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.core.urlresolvers import resolve
+from posts.views import home_page
+
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expect_html = render_to_string('home.html')
+        self.assertEqual(response.content, expect_html)
