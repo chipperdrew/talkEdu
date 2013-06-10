6c2a4cf305d5a0e55e91da92a69e85120824760b
Andrew Cook <chipperdrew@gmail.com>
2013-06-10 16:10:15 -0600

    Refactored redirect checks inot functions, added basic tests for new user form

--- funct_tests/tests.py
+++ funct_tests/tests.py
@@ -1,4 +1,3 @@
-#import unittest
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -12,6 +11,19 @@ class NewVisitorTests(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_redirect_after_link_click(self, link_text, expected_url_regex):
+        self.browser.find_element_by_link_text(link_text).click()
+        self.browser.implicitly_wait(3)
+        new_url = self.browser.current_url
+        self.assertRegexpMatches(new_url, expected_url_regex)
+
+    def check_for_redirect_after_button_click(self, button_name, expected_url_regex):
+        self.browser.find_element_by_name(button_name).click()
+        self.browser.implicitly_wait(3)
+        new_url = self.browser.current_url
+        self.assertRegexpMatches(new_url, expected_url_regex)
+        
+
     def test_home_page_has_proper_content_and_links(self):
         # Jim visits the home page of our site
         self.browser.get(self.live_server_url)
@@ -23,14 +35,11 @@ class NewVisitorTests(LiveServerTestCase):
         self.assertIn('Welcome', header_text)
 
         # Jim clicks on "Problems" link and is redirect to the problems page
-        self.browser.find_element_by_link_text("Problems").click()
-        self.browser.implicitly_wait(3)
-        problems_url = self.browser.current_url
-        self.assertRegexpMatches(problems_url, '/problems/$')
-
-        
-#    def test_problems_page_posts_and_saves_content(self):
+        self.check_for_redirect_after_link_click("Problems", '/problems/$')
+       
+    def test_problems_page_posts_and_saves_content(self):
         # The title of the problems page contains "Problems - "
+        self.browser.get(self.live_server_url+'/problems/')
         self.assertIn("Problems - ", self.browser.title)
 
         # A textarea is displayed, prompting "Type your thoughts here!"
@@ -58,23 +67,50 @@ class NewVisitorTests(LiveServerTestCase):
         self.assertIn('School is bad, mkay?', page_text)
         self.assertIn('I good at school', page_text)
 
-    def test_user_creation_and_authorization(self):
-        # Jim visits the homepage of the site (assume Jim is a user on the site)
-        self.browser.get(self.live_server_url)
+    def test_user_login_existance_and_authorization(self):
+        ######TODO - Update this test for authorization check
+        ######TODO - test login_box appears on EVERY page
 
-        # Jim sees a place to enter in his username and password
-        login_box= self.browser.find_element_by_id('id_login_box').text
+        # On the homepage, Jim sees a place to enter in his
+        # username and password
+        self.browser.get(self.live_server_url)
+        login_box = self.browser.find_element_by_id('id_login_box').text
         self.assertIn('Username:', login_box)
         self.assertIn('Password:', login_box)
 
-        # Jim enters his username and password into the appropriate boxes
+        # Jim see enters his username and password into the appropriate boxes
+        inputs = self.browser.find_elements_by_tag_name('input')
+        self.assertTrue(inputs.count, 2)
         user = self.browser.find_element_by_id('id_user_login')
         password = self.browser.find_element_by_id('id_pass_login')
         user.send_keys('Jim')
         password.send_keys('Password')
 
         # Jim clicks the 'Login' button and is returned to the current page
-        self.browser.find_element_by_name("login").click()
-        self.browser.implicitly_wait(3)
-        home_url = self.browser.current_url
-        self.assertRegexpMatches(home_url, '/$')
+        self.check_for_redirect_after_button_click("login", '/$')
+
+    def test_user_creation_form(self):
+        ######TODO - Try incorrect passwords, improper email format
+        ######TODO - username already taken, and check for verification email
+        
+        # On the homepage, Jim sees a place to create an account
+        self.browser.get(self.live_server_url)
+        login_box = self.browser.find_element_by_id('id_login_box').text
+        self.assertIn('Create Account', login_box)
+
+        # Jim clicks the link and is redirected to a create account page
+        self.check_for_redirect_after_link_click('Create Account', '/create_account/$')
+
+        # Jim sees 4 input boxes
+        inputs = self.browser.find_elements_by_tag_name('input')
+        self.assertTrue(inputs.count, 4)
+
+        # Jim enters in his information
+        inputs[0].send_keys('Jim')
+        inputs[1].send_keys('Password')
+        inputs[2].send_keys('Password')
+        inputs[3].send_keys('chipperdrew@gmail.com')
+
+        # Jim presses the "Create" button and is returned to home page
+        self.check_for_redirect_after_button_click("create", '/$')
+        
