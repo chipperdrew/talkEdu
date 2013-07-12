994a37ff996c47dc180e74799b641e9a96b46ed1
Andrew Cook <chipperdrew@gmail.com>
2013-07-12 17:55:37 -0600

    Search works! Thank you Elasticsearchgit statusgit status

--- talkEdu/settings/base.py
+++ talkEdu/settings/base.py
@@ -65,10 +65,9 @@ DISQUS_WEBSITE_SHORTNAME = 'YouTalkEdu'
 # Haystack
 HAYSTACK_CONNECTIONS = {
     'default': {
-        'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
-        'URL': 'http://localhost:8983/solr'
-        # ...or for multicore...
-        # 'URL': 'http://127.0.0.1:8983/solr/mysite',
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': 'haystack',
     },
 }
 
