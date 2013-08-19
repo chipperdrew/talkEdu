eaf533617fd44f549ec38b0868623829a2e48b54
Andrew Cook <chipperdrew@gmail.com>
2013-08-19 00:38:51 -0600

    Try again with spelling suggestions

--- talkEdu/settings/staging.py
+++ talkEdu/settings/staging.py
@@ -31,6 +31,7 @@ HAYSTACK_CONNECTIONS = {
         'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
         'URL': os.environ['SEARCHBOX_URL'],
         'INDEX_NAME': 'posts',
+        'INCLUDE_SPELLING': True, 
     },
 }
 HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
